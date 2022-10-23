bcf5788610b501b12a11269dcf1a39ef3029ec57
khlim <khlim@home.com>
2022-10-23 11:23:45 +0900

    init

---
+++ webpack.config.js
@@ -0,0 +1,71 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    index: './src/index.js',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'static/index.html',
+      filename : 'index.html',
+    }),
+    new HtmlWebpackPlugin({
+      template: 'view/write.html',
+      filename : 'write.html',
+    }),
+    new HtmlWebpackPlugin({
+      template: 'view/detail.html',
+      filename : 'detail.html',
+    }),
+  ],
+  devtool: 'inline-source-map',
+  devServer: {
+    static: './dist',
+    hot: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.html$/i,
+        loader: "html-loader",
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          "style-loader",
+          // Translates CSS into CommonJS
+          "css-loader",
+          // Compiles Sass to CSS
+          "sass-loader",
+        ],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      }
+    ],
+  },
+  output: {
+    // 실제 코드가 변경됐을 때만 hash값이 변경
+    filename: '[name].[contenthash].js',
+    path: path.resolve(__dirname, 'dist'),
+    assetModuleFilename: '[path][name]_[contenthash][ext]',
+    clean: true,
+  },
+  optimization: {
+    runtimeChunk: 'single',
+  },
+};
