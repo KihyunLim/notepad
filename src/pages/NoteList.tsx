ddc570f3fe88361bbb3bdb20be9d9803958cc673
khlim <khlim@home.com>
2022-11-07 00:11:32 +0900

    라우터 작업 중

---
+++ src/pages/NoteList.tsx
@@ -0,0 +1,190 @@
+import * as React from "react";
+
+export const NoteList = () => {
+  return (
+    <article className="post__grid">
+      <div className="post__cell">
+        <div className="post">
+          <div className="post__title">제목</div>
+          <img src={require("/src/assets/img/sample/sample1.jpg")} alt="" className="post__thumbnail"/*  onError="this.style.display='none';" */ />
+          <div className="post__content">
+            내용내뇨용 내용내뇨용 내용내뇨용 내용내뇨용 내용내뇨용 내용내뇨용 
+            내용내뇨용 내용내뇨용 내용내뇨용 내용내뇨용 내용내뇨용 내용내뇨용 내용내뇨용 
+            내용내뇨용 내용내뇨용 내용내뇨용 내용내뇨용 내용내뇨용 내용내뇨용 내용내뇨용 
+            내용내뇨용 내용내뇨용 내용내뇨용 
+          </div>
+          <div className="post__regist-date">2022-09-25</div>
+        </div>
+      </div>
+      <div className="post__cell">
+        <div className="post">
+          <div className="post__title">제목</div>
+          <img src={require("/src/assets/img/sample/sample1.jpg")} alt="" className="post__thumbnail"/*  onError="this.style.display='none';" */ />
+          <div className="post__content">
+            내용내뇨용 내용내뇨용 내용내뇨용 내용내뇨용 내용내뇨용 내용내뇨용 
+            내용내뇨용 내용내뇨용 내용내뇨용 내용내뇨용 내용내뇨용 내용내뇨용 내용내뇨용 
+            내용내뇨용 내용내뇨용 내용내뇨용 내용내뇨용 내용내뇨용 내용내뇨용 내용내뇨용 
+            내용내뇨용 내용내뇨용 내용내뇨용 
+          </div>
+          <div className="post__regist-date">2022-09-25</div>
+        </div>
+      </div>
+      <div className="post__cell">
+        <div className="post">
+          <div className="post__title">제목</div>
+          <img src={require("/src/assets/img/sample/sample1.jpg")} alt="" className="post__thumbnail"/*  onError="this.style.display='none';" */ />
+          <div className="post__content">
+            내용내뇨용 내용내뇨용 내용내뇨용 내용내뇨용 내용내뇨용 내용내뇨용 
+            내용내뇨용 내용내뇨용 내용내뇨용 내용내뇨용 내용내뇨용 내용내뇨용 내용내뇨용 
+            내용내뇨용 내용내뇨용 내용내뇨용 내용내뇨용 내용내뇨용 내용내뇨용 내용내뇨용 
+            내용내뇨용 내용내뇨용 내용내뇨용 
+          </div>
+          <div className="post__regist-date">2022-09-25</div>
+        </div>
+      </div>
+      <div className="post__cell">
+        <div className="post">
+          <div className="post__title">제목</div>
+          <img src={require("/src/assets/img/sample/sample1.jpg")} alt="" className="post__thumbnail"/*  onError="this.style.display='none';" */ />
+          <div className="post__content">
+            내용내뇨용 내용내뇨용 내용내뇨용 내용내뇨용 내용내뇨용 내용내뇨용 
+            내용내뇨용 내용내뇨용 내용내뇨용 내용내뇨용 내용내뇨용 내용내뇨용 내용내뇨용 
+            내용내뇨용 내용내뇨용 내용내뇨용 내용내뇨용 내용내뇨용 내용내뇨용 내용내뇨용 
+            내용내뇨용 내용내뇨용 내용내뇨용 
+          </div>
+          <div className="post__regist-date">2022-09-25</div>
+        </div>
+      </div>
+      <div className="post__cell">
+        <div className="post">
+          <div className="post__title">제목</div>
+          <img src={require("/src/assets/img/sample/sample1.jpg")} alt="" className="post__thumbnail"/*  onError="this.style.display='none';" */ />
+          <div className="post__content">
+            내용내뇨용 내용내뇨용 내용내뇨용 내용내뇨용 내용내뇨용 내용내뇨용 
+            내용내뇨용 내용내뇨용 내용내뇨용 내용내뇨용 내용내뇨용 내용내뇨용 내용내뇨용 
+            내용내뇨용 내용내뇨용 내용내뇨용 내용내뇨용 내용내뇨용 내용내뇨용 내용내뇨용 
+            내용내뇨용 내용내뇨용 내용내뇨용 
+          </div>
+          <div className="post__regist-date">2022-09-25</div>
+        </div>
+      </div>
+      <div className="post__cell">
+        <div className="post">
+          <div className="post__title">제목</div>
+          <img src={require("/src/assets/img/sample/sample1.jpg")} alt="" className="post__thumbnail"/*  onError="this.style.display='none';" */ />
+          <div className="post__content">
+            내용내뇨용 내용내뇨용 내용내뇨용 내용내뇨용 내용내뇨용 내용내뇨용 
+            내용내뇨용 내용내뇨용 내용내뇨용 내용내뇨용 내용내뇨용 내용내뇨용 내용내뇨용 
+            내용내뇨용 내용내뇨용 내용내뇨용 내용내뇨용 내용내뇨용 내용내뇨용 내용내뇨용 
+            내용내뇨용 내용내뇨용 내용내뇨용 
+          </div>
+          <div className="post__regist-date">2022-09-25</div>
+        </div>
+      </div>
+      <div className="post__cell">
+        <div className="post">
+          <div className="post__title">제목</div>
+          <img src={require("/src/assets/img/sample/sample1.jpg")} alt="" className="post__thumbnail"/*  onError="this.style.display='none';" */ />
+          <div className="post__content">
+            내용내뇨용 내용내뇨용 내용내뇨용 내용내뇨용 내용내뇨용 내용내뇨용 
+            내용내뇨용 내용내뇨용 내용내뇨용 내용내뇨용 내용내뇨용 내용내뇨용 내용내뇨용 
+            내용내뇨용 내용내뇨용 내용내뇨용 내용내뇨용 내용내뇨용 내용내뇨용 내용내뇨용 
+            내용내뇨용 내용내뇨용 내용내뇨용 
+          </div>
+          <div className="post__regist-date">2022-09-25</div>
+        </div>
+      </div>
+      <div className="post__cell">
+        <div className="post">
+          <div className="post__title">제목</div>
+          <img src={require("/src/assets/img/sample/sample1.jpg")} alt="" className="post__thumbnail"/*  onError="this.style.display='none';" */ />
+          <div className="post__content">
+            내용내뇨용 내용내뇨용 내용내뇨용 내용내뇨용 내용내뇨용 내용내뇨용 
+            내용내뇨용 내용내뇨용 내용내뇨용 내용내뇨용 내용내뇨용 내용내뇨용 내용내뇨용 
+            내용내뇨용 내용내뇨용 내용내뇨용 내용내뇨용 내용내뇨용 내용내뇨용 내용내뇨용 
+            내용내뇨용 내용내뇨용 내용내뇨용 
+          </div>
+          <div className="post__regist-date">2022-09-25</div>
+        </div>
+      </div>
+      <div className="post__cell">
+        <div className="post">
+          <div className="post__title">제목</div>
+          <img src={require("/src/assets/img/sample/sample1.jpg")} alt="" className="post__thumbnail"/*  onError="this.style.display='none';" */ />
+          <div className="post__content">
+            내용내뇨용 내용내뇨용 내용내뇨용 내용내뇨용 내용내뇨용 내용내뇨용 
+            내용내뇨용 내용내뇨용 내용내뇨용 내용내뇨용 내용내뇨용 내용내뇨용 내용내뇨용 
+            내용내뇨용 내용내뇨용 내용내뇨용 내용내뇨용 내용내뇨용 내용내뇨용 내용내뇨용 
+            내용내뇨용 내용내뇨용 내용내뇨용 
+          </div>
+          <div className="post__regist-date">2022-09-25</div>
+        </div>
+      </div>
+      <div className="post__cell">
+        <div className="post">
+          <div className="post__title">제목</div>
+          <img src={require("/src/assets/img/sample/sample1.jpg")} alt="" className="post__thumbnail"/*  onError="this.style.display='none';" */ />
+          <div className="post__content">
+            내용내뇨용 내용내뇨용 내용내뇨용 내용내뇨용 내용내뇨용 내용내뇨용 
+            내용내뇨용 내용내뇨용 내용내뇨용 내용내뇨용 내용내뇨용 내용내뇨용 내용내뇨용 
+            내용내뇨용 내용내뇨용 내용내뇨용 내용내뇨용 내용내뇨용 내용내뇨용 내용내뇨용 
+            내용내뇨용 내용내뇨용 내용내뇨용 
+          </div>
+          <div className="post__regist-date">2022-09-25</div>
+        </div>
+      </div>
+      <div className="post__cell">
+        <div className="post">
+          <div className="post__title">제목</div>
+          <img src={require("/src/assets/img/sample/sample1.jpg")} alt="" className="post__thumbnail"/*  onError="this.style.display='none';" */ />
+          <div className="post__content">
+            내용내뇨용 내용내뇨용 내용내뇨용 내용내뇨용 내용내뇨용 내용내뇨용 
+            내용내뇨용 내용내뇨용 내용내뇨용 내용내뇨용 내용내뇨용 내용내뇨용 내용내뇨용 
+            내용내뇨용 내용내뇨용 내용내뇨용 내용내뇨용 내용내뇨용 내용내뇨용 내용내뇨용 
+            내용내뇨용 내용내뇨용 내용내뇨용 
+          </div>
+          <div className="post__regist-date">2022-09-25</div>
+        </div>
+      </div>
+      <div className="post__cell">
+        <div className="post">
+          <div className="post__title">제목</div>
+          <img src={require("/src/assets/img/sample/sample1.jpg")} alt="" className="post__thumbnail"/*  onError="this.style.display='none';" */ />
+          <div className="post__content">
+            내용내뇨용 내용내뇨용 내용내뇨용 내용내뇨용 내용내뇨용 내용내뇨용 
+            내용내뇨용 내용내뇨용 내용내뇨용 내용내뇨용 내용내뇨용 내용내뇨용 내용내뇨용 
+            내용내뇨용 내용내뇨용 내용내뇨용 내용내뇨용 내용내뇨용 내용내뇨용 내용내뇨용 
+            내용내뇨용 내용내뇨용 내용내뇨용 
+          </div>
+          <div className="post__regist-date">2022-09-25</div>
+        </div>
+      </div>
+      <div className="post__cell">
+        <div className="post">
+          <div className="post__title">제목</div>
+          <img src={require("/src/assets/img/sample/sample1.jpg")} alt="" className="post__thumbnail"/*  onError="this.style.display='none';" */ />
+          <div className="post__content">
+            내용내뇨용 내용내뇨용 내용내뇨용 내용내뇨용 내용내뇨용 내용내뇨용 
+            내용내뇨용 내용내뇨용 내용내뇨용 내용내뇨용 내용내뇨용 내용내뇨용 내용내뇨용 
+            내용내뇨용 내용내뇨용 내용내뇨용 내용내뇨용 내용내뇨용 내용내뇨용 내용내뇨용 
+            내용내뇨용 내용내뇨용 내용내뇨용 
+          </div>
+          <div className="post__regist-date">2022-09-25</div>
+        </div>
+      </div>
+      <div className="post__cell">
+        <div className="post">
+          <div className="post__title">제목</div>
+          <img src={require("/src/assets/img/sample/sample1.jpg")} alt="" className="post__thumbnail"/*  onError="this.style.display='none';" */ />
+          <div className="post__content">
+            내용내뇨용 내용내뇨용 내용내뇨용 내용내뇨용 내용내뇨용 내용내뇨용 
+            내용내뇨용 내용내뇨용 내용내뇨용 내용내뇨용 내용내뇨용 내용내뇨용 내용내뇨용 
+            내용내뇨용 내용내뇨용 내용내뇨용 내용내뇨용 내용내뇨용 내용내뇨용 내용내뇨용 
+            내용내뇨용 내용내뇨용 내용내뇨용 
+          </div>
+          <div className="post__regist-date">2022-09-25</div>
+        </div>
+      </div>
+    </article>
+  );
+};
--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,38 +1,31 @@
 import * as React from "react";
-import { createBrowserRouter } from "react-router-dom";
+import { 
+  createBrowserRouter,
+} from "react-router-dom";
 
 import { App } from "../pages/App";
+import { NoteList } from "../pages/NoteList";
+import { NoteWrite } from "../pages/NoteWrite";
+import { NoteDetail } from "../pages/NoteDetail";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
     // errorElement: <ErrorPage />,
-    // loader: rootLoader,
-    // action: rootAction,
     children: [
-      // {
-      //   errorElement: <ErrorPage />,
-      //   children: [
-      //     { index: true, element: <Index /> },
-      //     {
-      //       path: "contacts/:contactId",
-      //       element: <Contact />,
-      //       loader: contactLoader,
-      //       action: contactAction,
-      //     },
-      //     {
-      //       path: "contacts/:contactId/edit",
-      //       element: <EditContact />,
-      //       loader: contactLoader,
-      //       action: editAction,
-      //     },
-      //     {
-      //       path: "contacts/:contactId/destroy",
-      //       action: destroyAction,
-      //     },
-      //   ]
-      // },
+      {
+        index: true,
+        element: <NoteList />,
+      },
+      {
+        path: "write",
+        element: <NoteWrite />,
+      },
+      {
+        path: "detail",
+        element: <NoteDetail />,
+      },
     ],
   },
 ]);
