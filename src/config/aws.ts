b406d5eb7bbfd1e879aa976049bbc388bee70243
khlim <khlim@corp.com>
2022-11-17 14:26:53 +0900

    aws 연동 모듈 파일 클래스로 전환

--- src/config/aws.ts
+++ src/config/aws.ts
@@ -1,31 +1,42 @@
 import { CognitoIdentityClient, CreateIdentityPoolCommand } from "@aws-sdk/client-cognito-identity";
 
-const client = new CognitoIdentityClient({ 
-  region: 'ap-northeast-2',
-  credentials: {
-    accessKeyId: process.env.ACCESS_KEY_ID as string,
-    secretAccessKey: process.env.SECRET_ACCESS_KEY as string,
+export class Aws {
+  private client: CognitoIdentityClient;
+
+  constructor() {
+    this.client = this.setClient();
+    this.testAWS();
   }
-});
 
-const params = {
-  AllowUnauthenticatedIdentities: true,
-  IdentityPoolName: process.env.IDENTITY_POOL_NAME as string
-};
-const command = new CreateIdentityPoolCommand(params);
+  setClient() {
+    return new CognitoIdentityClient({ 
+      region: process.env.REGION as string,
+      credentials: {
+        accessKeyId: process.env.ACCESS_KEY_ID as string,
+        secretAccessKey: process.env.SECRET_ACCESS_KEY as string,
+      }
+    });
+  }
 
-export const testCognito = async () => {
-  try {
-    const data = await client.send(command);
-    console.log('client send > then : ', data);
-    // process data.
-  } catch (error:any) {
-    // error handling.
-    console.log('client send > error : ', error);
-    // const { requestId, cfId, extendedRequestId } = error.$$metadata;
-    // console.log({ requestId, cfId, extendedRequestId });
-  } finally {
-    // finally.
-    console.log('client send > ?? > finally!!');
+  async testAWS() {
+    const params = {
+      AllowUnauthenticatedIdentities: true,
+      IdentityPoolName: process.env.IDENTITY_POOL_NAME as string
+    };
+    const command = new CreateIdentityPoolCommand(params);
+
+    try {
+      const data = await this.client.send(command);
+      console.log('client send > then : ', data);
+      // process data.
+    } catch (error:any) {
+      // error handling.
+      console.log('client send > error : ', error);
+      // const { requestId, cfId, extendedRequestId } = error.$$metadata;
+      // console.log({ requestId, cfId, extendedRequestId });
+    } finally {
+      // finally.
+      console.log('client send > ?? > finally!!');
+    }
   }
 }
--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,12 @@ import * as React from "react";
 import * as ReactDOM from "react-dom";
 import { RouterProvider } from "react-router-dom";
 
-import { testCognito } from './config/aws';
-
+import { Aws } from './config/Aws';
 import router from './router/index';
 
 import "./assets/css/app.scss";
 
-testCognito();
+// @ts-ignore
+const aws = new Aws();
 
 ReactDOM.render(<RouterProvider router={router} />, document.querySelector('#root'));
