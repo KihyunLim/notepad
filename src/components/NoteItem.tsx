e646549c0442c40974ec2cfe5ed820955acc51d3
khlim <khlim@corp.com>
2022-11-24 19:26:13 +0900

    북마크 색상 종류 최신화

--- src/components/NoteItem.tsx
+++ src/components/NoteItem.tsx
@@ -14,7 +14,8 @@ export const NoteItem = (props: NoteItemProps) => {
       <div className="post__cell">
         <div className="post">
           <div className="post__title">{note.title}</div>
-          <img src={note.thumbnailSrc} alt="" className="post__thumbnail" />
+          {/* 등록에 썸네일 등록을 안만들었네;; */}
+          {/* <img src={note.thumbnailSrc} alt="" className="post__thumbnail" /> */}
           <div className="post__content">{note.content}</div>
           <div className="post__regist-date">{note.registDate}</div>
         </div>
