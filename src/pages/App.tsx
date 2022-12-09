25e86e6069f81e6adbc08dab4257879856457989
khlim <khlim@corp.com>
2022-12-09 17:34:39 +0900

    axios 데이터 요청/응답 작업 시작

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,8 +1,14 @@
-import * as React from "react";
+import React, { useEffect } from "react";
+import { customAxois as axios } from "@/src/util/axios";
 
 import { AppLayout } from '@/src/components/AppLayout';
 
 export const App = () => {
+  useEffect(() => {
+    console.log('App.tsx loaded!!');
+    // const response = await axios.get('/note-list');
+  }, []);
+
   return (
     <>
       <AppLayout />
