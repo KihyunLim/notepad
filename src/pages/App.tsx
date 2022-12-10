031e033f219c95959011126f9fb4ce203e9e2b38
khlim <khlim@home.com>
2022-12-10 16:19:04 +0900

    db 연동 완료

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -6,7 +6,35 @@ import { AppLayout } from '@/src/components/AppLayout';
 export const App = () => {
   useEffect(() => {
     console.log('App.tsx loaded!!');
-    // const response = await axios.get('/note-list');
+    // useEffect 내부에 async를 사용하기에는 자연스러워 보이지 않음
+    /* const getNoteList = async () => {
+      console.log('request note list!!');
+      return await axios.get('/note-list');
+    }
+    const getBookmarkList = async () => {
+      console.log('request bookmark list!!');
+      return await axios.get('/bookmark-list');
+    }
+
+    const resNoteList = getNoteList().then((response) => {console.log('note done : ', response)});
+    const resBookmarkList = getBookmarkList().then((response) => {console.log('bookmark done : ', response)});
+
+    console.log('note list : ', resNoteList, ' / bookmark list : ', resBookmarkList); */
+
+    axios.get('/note-list')
+      .then(({ data }) => {
+        console.log('note list done : ', data);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+    axios.get('/bookmark-list')
+      .then(({ data }) => {
+        console.log('bookmark list done : ', data);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
   }, []);
 
   return (
